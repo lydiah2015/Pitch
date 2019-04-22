18c08ffe0f57ab0abdcf233b0228c4017234da72
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-04-22 11:37:35 +0300

    init Sqlalchemy object

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+db=SQLAlchemy()
 
 def create_app(config_name):
     app=Flask(__name__)
     app.config.from_object(config_options[config_name])
     Bootstrap(app)
+    db.init_app(app)
     # register blueprints
     from .main import main as main_blueprint
     from .auth import auth as auth_blueprint
