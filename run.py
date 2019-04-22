b03077c8e2948330994b8d13b0261abd1318c315
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-04-22 12:11:27 +0300

    migrations init

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 from app import create_app,db
 from flask_script import Manager, Server
 from flask_migrate import Migrate,MigrateCommand
+from app.models import User,Post,Comment,Upvote,Downvote
 
 app=create_app("development")
 manager=Manager(app)
 manager.add_command("runserver",Server(use_debugger=True))
-migrate=Migrate(app)
+migrate=Migrate(app,db)
 manager.add_command("db",MigrateCommand)
 
 @manager.command
