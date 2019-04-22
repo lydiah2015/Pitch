29773301a0cea941244f3f0ba80a61f8f993740a
Mitchelle Makini <mitchellemakini15@gmail.com>
2019-04-22 11:35:35 +0300

    created & registered blueprints - auth & main

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,9 @@ def create_app(config_name):
     app=Flask(__name__)
     app.config.from_object(config_options[config_name])
     Bootstrap(app)
+    # register blueprints
+    from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint)
     return app
